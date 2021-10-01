2aa8f339be5f9e63091a70578f25d8dee6a15abd
Paulo Camopy <paulorenato.cpb@gmail.com>
2021-09-30 22:00:57 -0300

    feat: :sparkles: added basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom'
+
 import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,17 +1,10 @@
-
-import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
-import googleIconImg from '../assets/images/google-icon.svg'
 import { Button } from '../components/Button';
 
-export function Home() {
-  const history = useHistory();
-
-  function navigateToNewRoom() {
-    history.push('/rooms/new')
-  }
+export function NewRoom() {
   return (
     <div className="flex items-stretch h-screen">
       <aside className="flex flex-col justify-center w-2/5 px-32 text-white bg-purple-500">
@@ -24,21 +17,14 @@ export function Home() {
         <div className="flex flex-col items-stretch w-full max-w-xs text-center">
           <img src={logoImg} alt="Letmeask" className="self-center" />
 
-          <button onClick={navigateToNewRoom} className="flex items-center justify-center h-12 gap-2 px-8 py-2 mt-16 mb-6 font-medium text-white bg-red-500 border-0 rounded-lg cursor-pointer hover:transition-opacity hover:opacity-90">
-            <img className="mr-2" src={googleIconImg} alt="Logo do Google" />
-            Crie sua sala com o Google
-          </button>
-
-          <div className="flex items-center justify-between mb-4 space-x-2">
-            <span className="flex-grow border-b-2 border-gray-300"></span>
-            <div>ou entre em uma sala</div>
-            <span className="flex-grow border-b-2 border-gray-300"></span>
-          </div>
+          <h2 className="mt-16 mb-6 text-2xl">Criar uma nova sala</h2>
 
           <form>
             <input className="w-full h-12 px-4 bg-white border border-gray-300 border-solid rounded-lg" type="text" placeholder="Digite o código da sala" />
-            <Button type="submit">Entrar na sala</Button>
+            <Button type="submit">Criar sala</Button>
           </form>
+
+          <p className="mt-4 text-sm text-gray-400">Quer entrar em uma sala existente? <Link className="text-pink-300" to="/">clique aqui</Link></p>
         </div>
       </main>
     </div>
